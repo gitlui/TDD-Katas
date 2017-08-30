9b82ea8cfab8f4aa1c1cee9f804fd39ab8e4a009
gitlui <none@none.com>
2017-08-31 00:14:16 +0200

    created first Test to generate FizzBuzz-Class

--- FizzBuzz/src/test/java/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -1,3 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public class FizzBuzzTest {
-    
+
+    @Test
+    public void itCanBeCreated() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertNotNull(fizzBuzz);
+    }
 }

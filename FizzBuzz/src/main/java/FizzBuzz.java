981d6d2ba994d7fb0956dde5c982bf4b0328eba9
gitlui <none@none.com>
2017-08-31 00:35:42 +0200

    replaced ocurrences in FizzBuzz-Class with consts

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -5,10 +5,10 @@ public class FizzBuzz {
     public String of(int input) {
         if (input > 0) {
             if (input % 3 == 0)
-                return "Fizz";
+                return FIZZ;
 
             if (input % 5 == 0)
-                return "Buzz";
+                return BUZZ;
         }
 
         return String.valueOf(input);

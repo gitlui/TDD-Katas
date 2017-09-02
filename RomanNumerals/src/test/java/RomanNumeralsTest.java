683d0870b1807524b9c14de5a2d666924ad247a0
gitlui <none@none.com>
2017-09-02 22:08:18 +0200

    Starting the RomanNumerals Kata

---
+++ RomanNumerals/src/test/java/RomanNumeralsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class RomanNumeralsTest {
+    @Test
+    public void itCanBeCreated() {
+        assertNotNull(new RomanNumerals());
+    }
+}

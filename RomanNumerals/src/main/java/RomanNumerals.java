76b4cbcba0b0a92d0e4476b29aa9d24b6b92948f
gitlui <none@none.com>
2017-09-02 22:14:30 +0200

    wrote a failing test for zero

--- RomanNumerals/src/main/java/RomanNumerals.java
+++ RomanNumerals/src/main/java/RomanNumerals.java
@@ -1,2 +1,5 @@
 public class RomanNumerals {
+    public String of(int number) {
+        return null;
+    }
 }
--- RomanNumerals/src/test/java/RomanNumeralsTest.java
+++ RomanNumerals/src/test/java/RomanNumeralsTest.java
@@ -1,10 +1,17 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class RomanNumeralsTest {
     @Test
     public void itCanBeCreated() {
         assertNotNull(new RomanNumerals());
+    }
+
+    @Test
+    public void aZeroReturnsAEmptyString() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("", romanNumerals.of(0));
     }
 }

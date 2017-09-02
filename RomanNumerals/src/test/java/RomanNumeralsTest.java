3cdf8823e4fd46a0d3b3bd841008e6cb8bdaa7e8
gitlui <none@none.com>
2017-09-02 22:15:56 +0200

    wrote a failing test for the one

--- RomanNumerals/src/test/java/RomanNumeralsTest.java
+++ RomanNumerals/src/test/java/RomanNumeralsTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 public class RomanNumeralsTest {
@@ -13,5 +14,11 @@ public class RomanNumeralsTest {
     public void aZeroReturnsAEmptyString() {
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("", romanNumerals.of(0));
+    }
+
+    @Test
+    public void aOneReturnsI() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("I", romanNumerals.of(1));
     }
 }

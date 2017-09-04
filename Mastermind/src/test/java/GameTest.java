d3f9d74177abd7318aeba99aaa0e83b20d3952a5
gitlui <none@none.com>
2017-09-04 23:18:26 +0200

    wrote a failing test for the Game class

---
+++ Mastermind/src/test/java/GameTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class GameTest {
+    @Test
+    public void itCanBeCreated() {
+        assertNotNull(new Game());
+    }
+}

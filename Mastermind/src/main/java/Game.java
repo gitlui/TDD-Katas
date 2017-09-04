fcf235f7c442e579305626f3ec7e660ffdf7fb76
gitlui <none@none.com>
2017-09-04 23:22:50 +0200

    wrote a failing test to get the function which returns the currentCodeToCrack

--- Mastermind/src/main/java/Game.java
+++ Mastermind/src/main/java/Game.java
@@ -1,2 +1,5 @@
 public class Game {
+    public Object currentCodeToCrack() {
+        return new Object();
+    }
 }
--- Mastermind/src/test/java/GameTest.java
+++ Mastermind/src/test/java/GameTest.java
@@ -1,10 +1,17 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class GameTest {
     @Test
     public void itCanBeCreated() {
         assertNotNull(new Game());
+    }
+
+    @Test
+    public void itStartsWithoutACode() {
+        Game game = new Game();
+        assertNull(game.currentCodeToCrack());
     }
 }

e6575299e90fa080cc0905616d82c0dbf382b9f3
gitlui <none@none.com>
2017-09-05 00:06:38 +0200

    moved the scorer init into setUp

--- Mastermind/src/test/java/ScorerTest.java
+++ Mastermind/src/test/java/ScorerTest.java
@@ -1,25 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ScorerTest {
+
+    private Scorer scorer;
+
+    @Before
+    public void setUp() throws Exception {
+        scorer = new Scorer();
+    }
+
     @Test
     public void itCanBeCreated() {
-        assertNotNull(new Scorer());
+        assertNotNull(scorer);
     }
 
     @Test
     public void itReturnsAScoreForATryAndACode() {
-        Scorer scorer = new Scorer();
-        PegColour[] code = new PegColour[] {PegColour.BLUE, PegColour.BLUE, PegColour.BLUE, PegColour.BLUE};
+        PegColour[] code = new PegColour[]{PegColour.BLUE, PegColour.BLUE, PegColour.BLUE, PegColour.BLUE};
         Score score = scorer.scoreTry(code, code);
         assertNotNull(score);
     }
 
     @Test
     public void itCanScoreOneRightColour() {
-        Scorer scorer = new Scorer();
         PegColour[] code = new PegColour[]{PegColour.BLUE, PegColour.YELLOW, PegColour.YELLOW, PegColour.YELLOW};
         PegColour[] codeTry = new PegColour[]{PegColour.BLUE, PegColour.RED, PegColour.RED, PegColour.RED};
         Score score = scorer.scoreTry(codeTry, code);

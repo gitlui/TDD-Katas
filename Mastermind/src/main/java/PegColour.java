c0d84f71218ae0e1b4fd356fb44a46725b96c522
gitlui <none@none.com>
2017-09-04 22:58:43 +0200

    made it pass by generating the enum and the value for BLUE

---
+++ Mastermind/src/main/java/PegColour.java
@@ -0,0 +1,3 @@
+public enum PegColour {
+    BLUE
+}
--- Mastermind/src/test/java/PegColourTest.java
+++ Mastermind/src/test/java/PegColourTest.java
@@ -1,8 +1,12 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
+
 public class PegColourTest {
     @Test
     public void itCanBeBlue() {
         assertNotNull(PegColour.BLUE);
     }
+
+    
 }

4c8aad31008c0613ac26c7664a307647af7ea49c
gitlui <none@none.com>
2017-09-04 22:53:58 +0200

    wrote a failing test to generate the GameMaster class

---
+++ Mastermind/src/test/java/GameMasterTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class GameMasterTest {
+    @Test
+    public void itCanBeCreated() {
+        assertNotNull(new GameMaster());
+    }
+}

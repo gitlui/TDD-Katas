139165eb489870a3002e5616f3cb06f03abf5409
gitlui <none@none.com>
2017-09-04 23:20:14 +0200

    gone back to the GameMasterTest to do some test cleaning I forgot

--- Mastermind/src/test/java/GameMasterTest.java
+++ Mastermind/src/test/java/GameMasterTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -5,27 +6,32 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GameMasterTest {
+
+    private GameMaster gm;
+
+    @Before
+    public void setUp() throws Exception {
+        gm = new GameMaster();
+    }
+
     @Test
     public void itCanBeCreated() {
-        assertNotNull(new GameMaster());
+        assertNotNull(gm);
     }
 
     @Test
     public void itCanGenerateACode() {
-        GameMaster gm = new GameMaster();
         assertNotNull(gm.generateCode());
     }
 
     @Test
     public void itCanGenerateACodeWithFourPegColours() {
-        GameMaster gm = new GameMaster();
         PegColour[] code = gm.generateCode();
         assertEquals(4, code.length);
     }
 
     @Test
     public void itCanGenerateACodeWhichActuallyContainsPegColours() {
-        GameMaster gm = new GameMaster();
         PegColour[] code = gm.generateCode();
         assertEquals(PegColour.class, code[0].getClass());
         assertEquals(PegColour.class, code[1].getClass());

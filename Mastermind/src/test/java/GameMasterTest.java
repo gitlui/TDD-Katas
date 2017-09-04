66ebd7d4b7209dbb5415a2d4355d504e52deb1eb
gitlui <none@none.com>
2017-09-04 23:02:09 +0200

    wrote a failing test for the "code" generation using 4 PegColours

--- Mastermind/src/test/java/GameMasterTest.java
+++ Mastermind/src/test/java/GameMasterTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GameMasterTest {
@@ -12,5 +13,12 @@ public class GameMasterTest {
     public void itCanGenerateACode() {
         GameMaster gm = new GameMaster();
         assertNotNull(gm.generateCode());
+    }
+
+    @Test
+    public void itCanGenerateACodeWithFourPegColours() {
+        GameMaster gm = new GameMaster();
+        PegColour[] code = gm.generateCode();
+        assertEquals(4, code.length);
     }
 }

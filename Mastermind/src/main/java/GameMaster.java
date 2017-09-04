51203b39c47f85cefa26abb0de5b31f39dd2938c
gitlui <none@none.com>
2017-09-04 23:17:20 +0200

    made it pass by filling the PegColour array with random PegColours

--- Mastermind/src/main/java/GameMaster.java
+++ Mastermind/src/main/java/GameMaster.java
@@ -1,6 +1,10 @@
 public class GameMaster {
     public PegColour[] generateCode() {
         PegColour[] code = new PegColour[4];
+
+        for (int i = 0; i < code.length; i++)
+            code[i] = PegColour.getRandomColour();
+
         return code;
     }
 }
